a0ffae9579d52460f0d2fc6764dad71856c605e0
Cole Marsteller, Miles Bradt and Robert Lee <cole+miles+robert@>
2019-02-04 10:18:24 -0800

    remove generated html and call canvas component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule
