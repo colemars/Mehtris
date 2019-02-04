e19b3b5a2bf835dc237cb3a451ab11c3835f6e97
Cole Marsteller <cgm32547@gmail.com>
2019-02-04 15:56:05 -0800

    add move methods to box model and add hit collision

--- src/app/models/square_tetromino.model.ts
+++ src/app/models/square_tetromino.model.ts
@@ -3,10 +3,10 @@ import * as p5 from 'p5';
 export class SquareTetromino {
   [x: string]: any;
   constructor(x, y, w, h) {
-    this.w = w;
-    this.h = h;
     this.x = x;
     this.y = y;
+    this.w = w;
+    this.h = h;
     this.posX = 50;
     this.posY = 50;
     this.id = 0;
@@ -23,11 +23,32 @@ export class SquareTetromino {
 
   }
 
-  hitHeight(p5, bodies) {
+  moveDown(p5, bodies) {
+    if (!(this.hit(p5, bodies, 0, 50))) {
+      this.posY += 50
+      return true;
+    } else return false;
+  }
+
+  moveLeft(p5, bodies) {
+    if (!(this.hit(p5, bodies, -50, 0))) {
+      this.posX -= 50
+      return true;
+    } else return false;
+  }
+
+  moveRight(p5, bodies) {
+    if (!(this.hit(p5, bodies, 50, 0))) {
+      this.posX += 50
+      return true;
+    } else return false;
+  }
+
+  hit(p5, bodies, side, down) {
     if (this.dead === false) {
       for (let i = 0; i < bodies.length; i++) {
         if (bodies[i].id != this.id) {
-          if((this.posX < bodies[i].posX + bodies[i].w) && (this.posX + this.w > bodies[i].posX) && (this.posY < bodies[i].posY + bodies[i].h) && (this.posY + this.h + 50 > bodies[i].posY)) {
+          if((this.posX + side < bodies[i].posX + bodies[i].w) && (this.posX + side + this.w > bodies[i].posX) && (this.posY + down < bodies[i].posY + bodies[i].h) && (this.posY + down + this.h > bodies[i].posY)) {
              console.log("hit")
              return true;
             }
@@ -35,4 +56,7 @@ export class SquareTetromino {
         }
       }
     }
+
+
+
   }
