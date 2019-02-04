cf29624d008a93bc6cce490313ec492c9d6e0c6f
Cole Marsteller <cgm32547@gmail.com>
2019-02-04 15:56:13 -0800

    incorporate movement with hit collission and only create a new box if box collides on y axis

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -20,8 +20,7 @@ export class CanvasComponent implements OnInit {
         bodies = [],
         count = 0;
 
-
-  const sketch = (s) => {
+    const sketch = (s) => {
 
     s.preload = () => {
 
@@ -44,31 +43,35 @@ export class CanvasComponent implements OnInit {
     s.keyPressed = () => {
       //moves piece within boundaries
       if((s.keyCode === s.RIGHT_ARROW) && (bodies[count].posX <= 400)) {
-        bodies[count].posX += 50;
-      } else if ((s.keyCode === s.LEFT_ARROW) && (bodies[count].posX >= 50)) {
-        bodies[count].posX -= 50;
-      } else if((s.keyCode === s.DOWN_ARROW) && (bodies[count].posY <= 900)) {
-        bodies[count].posY += 50;
-      }
-      // if (bodies.length > 1) {
-      //   if((bodies[0].posX < bodies[1].posX + bodies[1].w) && (bodies[0].posX + bodies[0].w > bodies[1].posX) && (bodies[0].posY < bodies[1].posY + bodies[1].h) && (bodies[0].posY + bodies[0].h > bodies[1].posY)) {
-      //     console.log("hit")
-      //   }
-      // // let test = s.collidePointRect(bodies[0].posX, bodies[0].posY, bodies[1].posX, bodies[1].posY, bodies[1].w, bodies[1].h)
-      //
-      // }
+        if (!(bodies[count].moveRight(s, bodies))) {
+          console.log("hello")
+          // bodies[count].dead = true;
+          // count++;
+          // bodies.push(new SquareTetromino(50,50,50,50))
+          // bodies[count].id += count;
 
-      // for (let i = 0; i < bodies.length; i++) {
-      //   // console.log(bodies[i].hit(s, bodies))
-      //   if (bodies[i].hitHeight(s, bodies, down, side)) {
-      //     // bodies[i].posY -= 50;
-      //     bodies[count].dead = true;
-      //     count++;
-      //     bodies.push(new SquareTetromino(50,50,50,50))
-      //     bodies[count].id += count;
-      //   }
-      //   // console.log(bodies[i].hit(s, bodies))
-      // }
+        }
+      } else if ((s.keyCode === s.LEFT_ARROW) && (bodies[count].posX >= 50)) {
+        if (!(bodies[count].moveLeft(s, bodies))) {
+          console.log("hello")
+          // bodies[count].dead = true;
+          // count++;
+          // bodies.push(new SquareTetromino(50,50,50,50))
+          // bodies[count].id += count;
+
+        }
+      } else if((s.keyCode === s.DOWN_ARROW) && (bodies[count].posY <= 900)) {
+
+        //if moveDown returns false, create new box
+        if (!(bodies[count].moveDown(s, bodies))) {
+          console.log("hello")
+          bodies[count].dead = true;
+          count++;
+          bodies.push(new SquareTetromino(50,50,50,50))
+          bodies[count].id += count;
+
+        }
+      }
 
       if (bodies[count].posY >= 950) {
         bodies[count].dead = true;
@@ -79,60 +82,28 @@ export class CanvasComponent implements OnInit {
       }
 
 
-      // console.log("X", bodies[count].posX)
-      // console.log("Y", bodies[count].posY)
-
 
     }
-    // s.centerCanvas = () => {
-    //   // canvas = s.createCanvas(200,400); //height of 24 to allow extra space for blocks to spawn
-    //   let x = (window.innerWidth - s.width) / 2;
-    //   let y = (window.innerHeight - s.height) / 2;
-    //   canvas.position(10, 10);
-    // }
+
 
     s.draw = () => {
       s.background('#7FB28A');
       s.noStroke(255);
 
-      // count = 0;
-      // if (bodies[count].posY >= 950) {
-      //   posY = 0;
-      //   count++;
-      //   bodies.push(new SquareTetromino(50,50,50,50, posX = 500, 0))
-      //   console.log(bodies);
-      // }
+
 
       for (let i = 0; i < bodies.length; i++) {
           bodies[i].show(s)
-          // bodies[i].hit(s, bodies)
-          // console.log(bodies[i].hit(s, bodies))
-        }
 
-        for (let i = 0; i < bodies.length; i++) {
-          // console.log(bodies[i].hit(s, bodies))
-          if (bodies[i].hitHeight(s, bodies)) {
-            // bodies[i].posY -= 50;
-            bodies[count].dead = true;
-            count++;
-            bodies.push(new SquareTetromino(50,50,50,50))
-            bodies[count].id += count;
-          }
-          // console.log(bodies[i].hit(s, bodies))
         }
 
 
-      // console.log('hit')
-
-
-      // s.background('black');
-      // s.noStroke(255);
-      // s.fill(170);
 
     };
   }
-  this.p5 = new p5(sketch);
 
+
+  this.p5 = new p5(sketch);
   }
 
 }
