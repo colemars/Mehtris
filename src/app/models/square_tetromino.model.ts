c9c0be7e223e1cff6638b2dcdf1c59bf1a9b3c96
Cole Marsteller, Miles Bradt and Robert Lee <cole+miles+robert@>
2019-02-04 16:23:09 -0800

    add working hit collision

--- src/app/models/square_tetromino.model.ts
+++ src/app/models/square_tetromino.model.ts
@@ -14,12 +14,12 @@ export class SquareTetromino {
   }
 
   show(p5) {
-     p5.push();
-     p5.translate(this.posX, this.posY);
-     // p5.rectMode(p5.CENTER);
-     p5.fill("#D8B6FF")
-     p5.rect(0,0,this.w, this.h);
-     p5.pop();
+    p5.push();
+    p5.translate(this.posX, this.posY);
+    // p5.rectMode(p5.CENTER);
+    p5.fill("#D8B6FF")
+    p5.rect(0,0,this.w, this.h);
+    p5.pop();
 
   }
 
@@ -27,12 +27,22 @@ export class SquareTetromino {
     if (this.dead === false) {
       for (let i = 0; i < bodies.length; i++) {
         if (bodies[i].id != this.id) {
-          if((this.posX < bodies[i].posX + bodies[i].w) && (this.posX + this.w > bodies[i].posX) && (this.posY < bodies[i].posY + bodies[i].h) && (this.posY + this.h + 50 > bodies[i].posY)) {
-             console.log("hit")
-             return true;
-            }
+          if (this.posY + this.h == bodies[i].posY && this.posX + this.w == bodies[i].posX + bodies[i].w) {
+            console.log("Hit top")
+            return true;
+          } else if ((this.posX + this.w == bodies[i].posX) && bodies[i].posY + bodies[i].h == this.posY + this.h) {
+            // collision detected!
+            console.log("Hit left side")
+
+          } else if ((bodies[i].posX + bodies[i].w == this.posX && bodies[i].posY + bodies[i].h == this.posY + this.h)) {
+            console.log("Hit right side")
           }
+          // if((this.posX < bodies[i].posX + bodies[i].w) && (this.posX + this.w > bodies[i].posX) && (this.posY < bodies[i].posY + bodies[i].h) && (this.posY + this.h + 50 > bodies[i].posY)) {
+          //    console.log("hit")
+          //    return true;
+          //   }
         }
       }
     }
   }
+}
