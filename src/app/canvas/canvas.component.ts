6e0e6e3bfa3882829cbbaf2a0620d0db8bc9ba53
Student <example@example.com>
2019-02-05 14:51:30 -0800

    fix movement bug

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -19,126 +19,56 @@ export class CanvasComponent implements OnInit {
   ngOnInit() {
 
     let canvas,
-        square,
-        bodies = [],
-        count = 0;
+    square,
+    bodies = [],
+    count = 0;
 
     const sketch = (s) => {
 
-    s.preload = () => {
+      s.preload = () => {
 
-    }
-
-
-    s.setup = () => {
-
-      //builds p5 canvas
-      let canvas = s.createCanvas(500, 1000);
-      let x = (window.innerWidth - s.width) / 2;
-      let y = (window.innerHeight - s.height) / 2;
-      canvas.position(x,y);
-      // s.background(255, 0, 200);
-
-      bodies.push(new SquareTetromino(50,50,50,50))
-    }
+      }
 
 
-    s.keyPressed = () => {
-      //moves piece within boundaries
-      if((s.keyCode === s.RIGHT_ARROW) && (this.body.posX <= 400)) {
+      s.setup = () => {
+
+        //builds p5 canvas
+        let canvas = s.createCanvas(500, 1000);
+        let x = (window.innerWidth - s.width) / 2;
+        let y = (window.innerHeight - s.height) / 2;
+        canvas.position(x,y);
+        // s.background(255, 0, 200);
+
+        bodies.push(new SquareTetromino(200,-50,50,50))
+        // downward tick
+        setInterval(() => {
+          if (!(this.body.moveDown(s, bodies)) || this.body.posY >= 950) {
+            this.body.dead = true;
+            count++;
+            bodies.push(new SquareTetromino(200,-50,50,50))
+            this.body.id += count;
+          }
+        }, 250);
+      }
+
+
+      s.keyPressed = () => {
+        //moves piece within boundaries
+        if((s.keyCode === s.RIGHT_ARROW) && (this.body.posX <= 400)) {
         if (!(this.body.moveRight(s, bodies))) {
-          console.log("hello")
-          // bodies[count].dead = true;
-          // count++;
-          // bodies.push(new SquareTetromino(50,50,50,50))
-          // bodies[count].id += count;
-
         }
       } else if ((s.keyCode === s.LEFT_ARROW) && (this.body.posX >= 50)) {
         if (!(this.body.moveLeft(s, bodies))) {
-          console.log("hello")
-          // bodies[count].dead = true;
-          // count++;
-          // bodies.push(new SquareTetromino(50,50,50,50))
-          // bodies[count].id += count;
-
         }
       } else if((s.keyCode === s.DOWN_ARROW) && (this.body.posY <= 900)) {
-
-        //if moveDown returns false, create new box
         if (!(this.body.moveDown(s, bodies)) || this.body.posY >= 950) {
-          console.log("hello")
           this.body.dead = true;
           count++;
-          bodies.push(new SquareTetromino(50,50,50,50))
+          bodies.push(new SquareTetromino(200,-50,50,50))
           this.body.id += count;
 
         }
       }
-
-      // if (bodies[count].posY >= 950) {
-      //   bodies[count].dead = true;
-      //   count++;
-      //   bodies.push(new SquareTetromino(50,50,50,50))
-      //   bodies[count].id += count;
-      //   console.log(bodies[count].id);
-      // }
-
-      let deadPieceArray = [];
-        let deadPieceSortedArray = [];
-        console.log("Dead pieces:", deadPieceArray);
-        for (let i = 0; i < bodies.length; i++) {
-          if(bodies[i].dead === true) {
-            deadPieceArray.push(bodies[i]);
-            deadPieceSortedArray.push(bodies[i]);
-          }
-        }
-
-        let deadPiecePosY = [];
-        let deadPieceId = [];
-
-        function sortNumbers(a, b) {
-          return a - b;
-        }
-
-        let testFunction = deadPieceSortedArray.sort(function(a, b) {
-          return a.posY - b.posY;
-        });
-
-
-        for (let i = 0; i < deadPieceArray.length; i++) {
-          deadPiecePosY.push(deadPieceArray[i].posY);
-          deadPieceId.push(deadPieceArray[i].id);
-        }
-
-        let posYTrack = [], AmmountOfPosY = [], prev;
-        for (let i = 0; i < testFunction.length; i++) {
-            if (testFunction[i].posY !== prev) {
-            posYTrack.push(testFunction[i].posY);
-            AmmountOfPosY.push(1);
-          } else {
-            AmmountOfPosY[AmmountOfPosY.length-1]++;
-          }
-          prev = testFunction[i].posY;
-        }
-
-        console.log(["Y Position:", posYTrack, "Amount of:", AmmountOfPosY])
-
-        let linesOfArray = [];
-        for (let i = 0; i < AmmountOfPosY.length; i++) {
-          if (AmmountOfPosY[i] === 10) {
-            console.log("Line on Y position:", posYTrack[i])
-            let line = posYTrack[i]
-            for (let j = 0; j < deadPieceArray.length; j++) {
-              if(deadPieceArray[j].posY === line) {
-                bodies.splice(bodies.indexOf(deadPieceArray[j]), 1)
-              }
-            }
-          }
-        }
-        console.log("Lines:", linesOfArray)
-
-
 
     }
 
@@ -148,25 +78,76 @@ export class CanvasComponent implements OnInit {
       s.noStroke(255);
 
 
-
       for (let i = 0; i < bodies.length; i++) {
-          bodies[i].show(s)
+        bodies[i].show(s)
       }
 
       for (let i = 0; i < bodies.length; i++) {
-          if (bodies[i].dead === false) {
-            this.body = bodies[i];
+        if (bodies[i].dead === false) {
+          this.body = bodies[i];
+        }
+      }
+
+      let deadPieceArray = [];
+      let deadPieceSortedArray = [];
+      // console.log("Dead pieces:", deadPieceArray);
+      for (let i = 0; i < bodies.length; i++) {
+      if(bodies[i].dead === true) {
+        deadPieceArray.push(bodies[i]);
+        deadPieceSortedArray.push(bodies[i]);
+      }
+    }
+
+    let deadPiecePosY = [];
+    let deadPieceId = [];
+
+    function sortNumbers(a, b) {
+      return a - b;
+    }
+
+    let testFunction = deadPieceSortedArray.sort(function(a, b) {
+      return a.posY - b.posY;
+    });
+
+
+    for (let i = 0; i < deadPieceArray.length; i++) {
+      deadPiecePosY.push(deadPieceArray[i].posY);
+      deadPieceId.push(deadPieceArray[i].id);
+    }
+
+    let posYTrack = [], AmmountOfPosY = [], prev;
+    for (let i = 0; i < testFunction.length; i++) {
+      if (testFunction[i].posY !== prev) {
+        posYTrack.push(testFunction[i].posY);
+        AmmountOfPosY.push(1);
+      } else {
+        AmmountOfPosY[AmmountOfPosY.length-1]++;
+      }
+      prev = testFunction[i].posY;
+    }
+
+    // console.log(["Y Position:", posYTrack, "Amount of:", AmmountOfPosY])
+
+    let linesOfArray = [];
+    for (let i = 0; i < AmmountOfPosY.length; i++) {
+      if (AmmountOfPosY[i] === 10) {
+        // console.log("Line on Y position:", posYTrack[i])
+        let line = posYTrack[i]
+        for (let j = 0; j < deadPieceArray.length; j++) {
+          if(deadPieceArray[j].posY === line) {
+            bodies.splice(bodies.indexOf(deadPieceArray[j]), 1)
           }
         }
+      }
+    }
+    // console.log("Lines:", linesOfArray)
 
 
+  };
+}
 
 
-    };
-  }
-
-
-  this.p5 = new p5(sketch);
-  }
+this.p5 = new p5(sketch);
+}
 
 }
