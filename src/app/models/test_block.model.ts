6e4f43e6eb777af69ec47a9b4ddf99102ae62fc6
Cole Marsteller <cgm32547@gmail.com>
2019-02-09 04:00:36 -0800

    add color variable to block

--- src/app/models/test_block.model.ts
+++ src/app/models/test_block.model.ts
@@ -1,15 +1,17 @@
 export class TestBlock {
   [x: string]: any;
-	constructor(x, y, w) { // no need for h, because it's square
+	constructor(x, y, w, color) { // no need for h, because it's square
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = w;
+    this.color = color;
     this.scored = false;
 	}
 
 	show(p5) {
     p5.stroke('black')
+    p5.fill(this.color)
 		p5.rect(this.x, this.y, this.w, this.h);
 	}
 }
