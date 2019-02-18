849f12d5548761734bb125e5c296b67ac34c44bb
Cole Marsteller <cgm32547@gmail.com>
2019-02-17 17:44:17 -0800

    build off screen(-50y,-100y) rows

--- src/app/models/game-array.model.ts
+++ src/app/models/game-array.model.ts
@@ -3,6 +3,11 @@ export class GameArray {
   }
 
   static gameState(gameArray, bodies, s) {
+    //builds off screen matrices
+    for (let i=1;i<3; i++) {
+        gameArray.push([[0,0,-100/i,0],[0, 50,-100/i,0],[0, 100,-100/i,0],[0, 150,-100/i,0],[0, 200,-100/i,0],[0, 250,-100/i,0],[0, 300,-100/i,0],[0, 350,-100/i,0],[0, 400,-100/i,0],[0, 450,-100/i,0]]);
+    }
+    //builds on screen matrices
     for(let i=0; i<20; i++){
       gameArray.push([[0,0,s.height/20*i,0],[0, 50,s.height/20*i,0],[0, 100,s.height/20*i,0],[0, 150,s.height/20*i,0],[0, 200,s.height/20*i,0],[0, 250,s.height/20*i,0],[0, 300,s.height/20*i,0],[0, 350,s.height/20*i,0],[0, 400,s.height/20*i,0],[0, 450,s.height/20*i,0]]);
     }
